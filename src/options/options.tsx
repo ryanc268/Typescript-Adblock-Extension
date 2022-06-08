59758551681151e5b8135d1c531094a40150860b
Ryan Coppa <ryan.tcoppa@gmail.com>
2022-06-08 02:44:23 -0400

    setting up rules skeleton for blocking ads

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './options.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./options.css";
 
 const App: React.FC<{}> = () => {
   return (
     <div>
       <img src="icon.png" />
     </div>
-  )
-}
+  );
+};
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
